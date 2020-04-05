2e6882ce484f163961456853e4d16ad2c98c57d2
Kyle Middleton <kylemid321@gmail.com>
2020-04-05 16:42:51 -0600

    initial commit

---
+++ furnace.adb
@@ -0,0 +1,24 @@
+$furnace_control
+ binary
+ b0 "Off"
+ b1 "On"
+ value Off
+ out
+$furnace_status
+ binary
+ b0 "Unavailable"
+ b1 "Available"
+ value Unavailable
+ in
+$room_1_damper
+ binary
+ b0 "Closed"
+ b1 "Open"
+ value Open
+ out
+$room_2_damper
+ binary
+ b0 "Closed"
+ b1 "Open"
+ value Open
+ out
