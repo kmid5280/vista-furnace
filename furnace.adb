7e319a8432305f1506a9ecaf665a571c098357ee
Kyle Middleton <kylemid321@gmail.com>
2020-04-05 16:48:01 -0600

    add integer and real channels

--- furnace.adb
+++ furnace.adb
@@ -22,3 +22,28 @@ $room_2_damper
  b1 "Open"
  value Open
  out
+$room_1_thermostat
+ integer
+ units "degrees"
+ label "thermostat setting room 1"
+ out
+$room_2_thermostat
+ integer
+ units "degrees"
+ label "thermostat setting room 2"
+ out
+$room_1_temp
+ real
+ units "degrees"
+ label "temperature setting room 1"
+ in
+$room_2_temp
+ real
+ units "degrees"
+ label "temperature setting room 2"
+ in
+$furnace_temp
+ real
+ units "degrees"
+ label "furnace temperature"
+ in
